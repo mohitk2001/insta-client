d1e4ce9ecff5635ea42cd469eb00f81786b658cc
mohit <mohitkumar.cse19@chitkarauniversity.edu.in>
2022-01-04 19:28:08 +0530

    v1

--- src/Components/Authentication.js
+++ src/Components/Authentication.js
@@ -5,7 +5,9 @@ import "./Authentication.css";
 import { Link } from "react-router-dom";
 import axios from "./axios";
 import { useHistory } from "react-router";
-
+import Loader from "./Loader";
+import "./Edit.css";
+import $ from "jquery";
 function Authentication() {
   const { setIslogged } = useContext(AuthContext);
   const [email, setEmail] = useState("");
@@ -18,6 +20,8 @@ function Authentication() {
       email: email,
       password: password,
     };
+    $(".loader").addClass("activeLoader");
+    $(".auth").attr("id", "whenlodingbackBlur");
     axios
       .post("/user/login", { obj })
       .then((res) => {
@@ -30,7 +34,8 @@ function Authentication() {
             id: res.data._id,
             loginstatus: true,
           });
-
+          $(".loader").removeClass("activeLoader");
+          $(".auth").removeAttr("id", "whenlodingbackBlur");
           history.push("/");
         }
       })
@@ -39,44 +44,47 @@ function Authentication() {
       });
   };
   return (
-    <div className="auth">
-      <div className="authentication">
-        <form>
-          <center>
-            <Link to="/">
-              <img
-                src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
-                alt="logo"
-                className="logo_form"
-              />
+    <>
+      <div className="auth">
+        <div className="authentication">
+          <form>
+            <center>
+              <Link to="/">
+                <img
+                  src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+                  alt="logo"
+                  className="logo_form"
+                />
+              </Link>
+            </center>
+            <input
+              type="email"
+              placeholder="email..."
+              required
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <input
+              type="password"
+              placeholder="password..."
+              required
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <button type="submit" onClick={login}>
+              Log in
+            </button>
+          </form>
+          <p>
+            Don't have an account ?{" "}
+            <Link to="/signup">
+              <strong> Sign Up </strong>
             </Link>
-          </center>
-          <input
-            type="email"
-            placeholder="email..."
-            required
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <input
-            type="password"
-            placeholder="password..."
-            required
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <button type="submit" onClick={login}>
-            Log in
-          </button>
-        </form>
-        <p>
-          Don't have an account ?{" "}
-          <Link to="/signup">
-            <strong> Sign Up </strong>
-          </Link>
-        </p>
+          </p>
+        </div>
       </div>
-    </div>
+      <Loader />
+    </>
   );
 }
 
--- src/Components/SignUp.js
+++ src/Components/SignUp.js
@@ -3,15 +3,20 @@ import "./SignUp.css";
 import { Link } from "react-router-dom";
 import axios from "./axios";
 import { useHistory } from "react-router";
+import Loader from "./Loader";
+import "./Edit.css";
+import $ from "jquery";
 function SignUp() {
   const [name, setName] = useState("");
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const history=useHistory();
+  const history = useHistory();
 
   const register = async (e) => {
     e.preventDefault();
+    $(".loader").addClass("activeLoader");
+    $(".signUp").attr("id", "whenlodingbackBlur");
     const obj = {
       name: name,
       email: email,
@@ -22,59 +27,66 @@ function SignUp() {
     await axios
       .post("/user/signUp", { obj })
       .then((res) => {
-        history.push("/login")
+        $(".loader").removeClass("activeLoader");
+        $(".signUp").removeAttr("id", "whenlodingbackBlur");
+        history.push("/login");
       })
       .catch((err) => {
         console.log(err);
       });
-    setName("")
-    setEmail("")
-    setPassword("")
-    setUsername("")
+    setName("");
+    setEmail("");
+    setPassword("");
+    setUsername("");
   };
   return (
-    <div className="signUp">
-      <form>
-        <center>
-          <Link to="/">
-            <img
-              src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
-              alt="logo"
-              className="logo"
-            />
-          </Link>
-        </center>
-        <input
-          type="text"
-          placeholder="full name..."
-          required
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-        <input
-          type="text"
-          placeholder="username..."
-          required
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-        <input
-          type="email"
-          placeholder="email..."
-          required
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <input
-          type="password"
-          placeholder="password..."
-          required
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button type="submit" onClick={register}>Sign Up </button>
-      </form>
-    </div>
+    <>
+      <div className="signUp">
+        <form>
+          <center>
+            <Link to="/">
+              <img
+                src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+                alt="logo"
+                className="logo"
+              />
+            </Link>
+          </center>
+          <input
+            type="text"
+            placeholder="full name..."
+            required
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="username..."
+            required
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <input
+            type="email"
+            placeholder="email..."
+            required
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="password..."
+            required
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button type="submit" onClick={register}>
+            Sign Up{" "}
+          </button>
+        </form>
+      </div>
+      <Loader />
+    </>
   );
 }
 
